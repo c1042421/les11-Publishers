aa3de9c28f8f552d911394e1da7ea1c8cc43e28e
c1042421 <c1042421@G-D103-14>
2017-12-19 11:44:59 +0100

    added Partial class Employee

---
+++ les11-Oef1/EmployeeExt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace les11_Oef1
+{
+    partial class Employee
+    {
+        public override bool Equals(object obj)
+        {
+            var employee = obj as Employee;
+            return employee != null &&
+                   emp_id == employee.emp_id;
+        }
+    }
+}
